84c3d44650b426bb72367bd637e205a8d47d415d
hualingfeng <tc_hualingfeng@163.com>
2015-10-03 15:36:52 +0800

    create a application

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path    = require('path');
+var ejs     = require('ejs');
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = require('socket.io').listen(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/',function (req,res) {
+  res.render('chat',{title:'chat room'});
+});
+
+
+io.on('connection', function (socket) {
+  
+});
+
+server.listen('8081',function(){
+  console.log('--------socket listening--------');
+});
