0400d92938eb1eff9b418f75432a5d87e3c562eb
hualingfeng <tc_hualingfeng@163.com>
2015-10-05 22:43:16 +0800

    login&show message&user config

--- index.js
+++ index.js
@@ -11,10 +11,49 @@ app.get('/',function (req,res) {
   res.render('chat',{title:'聊天室'});
 });
 
-
 io.on('connection', function (socket) {
-  
+  //构造客户端对象
+  var client = {
+    socket:socket,
+    name:false,
+    color:getColor()
+  };
+
+  socket.on('newuser',function(msg) {
+    var data = {
+      time:getTime(),
+      color:getColor(),
+      name:msg.name,
+      type:'welcome'
+    };
+    socket.emit('system',data);
+    socket.broadcast.emit('system',data);
+  });
+
+  //发送消息监听
+  socket.on('message',function(msg){
+    var data ={
+     time:getTime(),
+     msg:msg
+    };
+    socket.emit('message',data);
+    socket.broadcast.emit('message',data);
+  });
+
 });
+
+//时间
+var getTime=function(){
+  var date = new Date();
+  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+}
+
+//颜色
+var getColor=function(){
+  var colors = ['aliceblue','antiquewhite','aqua','aquamarine','pink','red','green',
+                'orange','blue','blueviolet','brown','burlywood','cadetblue'];
+  return colors[Math.round(Math.random() * 10000 % colors.length)];
+}
 
 server.listen('8081',function(){
   console.log('--------socket listening--------');
