a6954b3ed22b063ff68b4ab49915f5d2bcbf2728
hualingfeng <tc_hualingfeng@163.com>
2015-10-07 13:15:33 +0800

    final

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
-var express = require('express');
-var path    = require('path');
-var ejs     = require('ejs');
-var app     = express();
-var server  = require('http').createServer(app);
-var io      = require('socket.io').listen(server);
+var express         = require('express');
+var path            = require('path');
+var ejs             = require('ejs');
+var config          = require('config');
+var bodyParser      = require('body-parser');
+var app             = express();
+var server          = require('http').createServer(app);
+var io              = require('socket.io').listen(server);
 
+var client = {};
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/',function (req,res) {
-  res.render('chat',{title:'聊天室'});
+  res.render('login',{alarm:''});
+});
+
+app.post('/login',function (req,res) {
+  if(config.has(req.body.name)){
+    var userInfo = config.get(req.body.name);
+    if(userInfo.pwd == req.body.pwd){
+      client['name'] = req.body.name;
+      client['rname'] = userInfo.name;
+      client['id'] = userInfo.id;
+      client['icon'] = userInfo.icon
+      res.redirect('/chat');
+    }
+    else{
+      res.render('login',{alarm:'用户名或密码错误'});
+    }
+  }else{
+  	res.render('login',{alarm:'用户名或密码错误'});
+  }
+});
+
+app.get('/chat',function (req,res) {
+	res.render('chat',{title:'聊天室',client:client});
 });
 
 io.on('connection', function (socket) {
-  //构造客户端对象
-  var client = {
-    socket:socket,
-    name:false,
-    color:getColor()
-  };
-
-  socket.on('newuser',function(msg) {
+  socket.on('newuser',function() {
     var data = {
-      time:getTime(),
-      color:getColor(),
-      name:msg.name,
+      name:client['rname']||'',
       type:'welcome'
     };
-    socket.emit('system',data);
-    socket.broadcast.emit('system',data);
+    if(data.name){
+	  socket.emit('system',data);
+	  socket.broadcast.emit('system',data);
+    }
   });
 
   //发送消息监听
   socket.on('message',function(msg){
     var data ={
-     time:getTime(),
+     id:client.id,
+     name:client.name,
+     rname:client.rname,
+     icon:client.icon,
      msg:msg
     };
     socket.emit('message',data);
@@ -42,18 +66,12 @@ io.on('connection', function (socket) {
 
 });
 
-//时间
-var getTime=function(){
-  var date = new Date();
-  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-}
+// //时间
+// var getTime=function(){
+//   var date = new Date();
+//   return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+// }
 
-//颜色
-var getColor=function(){
-  var colors = ['aliceblue','antiquewhite','aqua','aquamarine','pink','red','green',
-                'orange','blue','blueviolet','brown','burlywood','cadetblue'];
-  return colors[Math.round(Math.random() * 10000 % colors.length)];
-}
 
 server.listen('8081',function(){
   console.log('--------socket listening--------');
